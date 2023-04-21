ceb115c2f611ebe20052e0d37d2deb6a3591fc2f
masterege <halitebiller96@gmail.com>
2023-04-21 21:32:52 +0200

    Alpha Version

---
+++ app/models.py
@@ -0,0 +1,79 @@
+from django.db import models
+from django.urls import reverse
+from django.core.validators import FileExtensionValidator
+from django.contrib.auth.models import AbstractUser
+from django.db.models import Q
+
+news = "News"
+events = "Events"
+
+POST = ((news, "News"), (events, "Events"),
+       )
+
+first = "First" 
+second = "Second"
+
+semester = ((first, "First"), (second, "Second"),
+            )
+
+# News and Events Queries
+
+class NewsAndEventsQueries(models.query.QuerySet):
+    def search(self, query):
+        lookups = (Q(title_icontains = query) | 
+                  Q(summary_icontains = query) |
+                  Q(posted_as_icontains = query)
+                  )
+        return self.filter(lookups).distinct()
+    
+# News and Events Operations Management(Will return to NewsAndEventQueries to find the selected keywords)
+
+class NewsAndEventManagement(models.Manager):
+    def get_queryset(self):
+        return NewsAndEventsQueries(self.model, using = self._db)
+    
+    def all(self):
+        return self.get_queryset()
+
+    def get_by_id(self, id):
+        qs = self.get_queryset().filter(id = id) # NewsAndEvents.objects == self.get_queryset()
+        if qs.count() == 1:
+            return qs.first()
+        return None
+
+    def search(self, query):
+        return self.get_queryset().search(query)
+    
+# News and Events Model(Will return the objects to NewsAndEventsManagement)
+
+class NewsAndEvents(models.Model):
+    title = models.CharField(max_length = 120, null = True)
+    summary = models.TextField(max_length = 200, blank = True, null = True)
+    posted_as = models.CharField(choices = POST, max_length = 10)
+    update_date =  models.DateTimeField(auto_now=True, auto_now_add=False, null=True)
+    upload_time = models.DateTimeField(auto_now=False, auto_now_add=True, null=True)
+
+    objects = NewsAndEventManagement() # Returning the objects to Event Management
+
+    def __str__(self):
+        return self.title
+    
+# Session Model
+
+class Session(models.Model):
+    session = models.CharField(max_length = 200, unique = True)
+    is_current_session = models.BooleanField(default = False, blank = True, null = True)
+    next_session_begins = models.DateField(blank = True, null = True)
+
+    def __str__(self):
+        return self.session
+
+
+class Semester(models.Model):
+    semester = models.CharField(max_length = 10, choices = semester, blank=True)
+    is_current_semester = models.BooleanField(default=False, blank=True, null=True)
+    session = models.ForeignKey(Session, on_delete=models.CASCADE, blank=True, null=True)
+    next_semester_begins = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.semester
---
+++ home/asgi.py
@@ -0,0 +1,22 @@
+"""
+ASGI config for home project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+import django
+from channels.http import AsgiHandler
+from channels.routing import ProtocolTypeRouter
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SMS.settings')
+django.setup()
+
+application = ProtocolTypeRouter({
+  "http": AsgiHandler(),
+  # Just HTTP for now. (We can add other protocols later.)
+})
